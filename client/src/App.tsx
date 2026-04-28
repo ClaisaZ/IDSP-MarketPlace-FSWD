6721962a76748d3d0a3fc024d2255d42f12c42b3
Shervin Babaei <shervinb22@gmail.com>
2026-04-27 17:47:25 -0700

    chore: initial project setup for client/server

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <>{/* all stuff should go here --> switches between screens */}</>;
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(PORT, () => console.log(`App listening at ${PORT}`));
