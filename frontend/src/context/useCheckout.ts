3c48db02a89738250de4120c53b89c36d3f0e05e
Ky Thai <tuocky2000@gmail.com>
2026-04-29 12:14:48 -0700

    Revert "Shervin tasks (#19)" (#20)
    This reverts commit 9e79d79dd5282c2bfb3e9dbf33e53ee7b063957b.

--- frontend/src/context/useCheckout.ts
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { CheckoutContext } from "./CheckoutContext";
-
-export function useCheckout() {
-  const context = useContext(CheckoutContext);
-  if (!context) throw new Error("useCheckout must be used within a CheckoutProvider");
-  return context;
-}
