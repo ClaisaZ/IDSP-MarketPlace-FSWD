ccfb07aef76ede1aa87d26223458d1fbc11130c8
Shervin Babaei <shervinb22@gmail.com>
2026-05-04 12:18:53 -0700

    dotenv move up 1st line

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 const mongoose = require("mongoose");
 const profileRoute = require("./routes/profile");
 const registerCourseRoute = require("./routes/register"); // Course Registration
