70af0eb9f25a7dfc65913a5500b34cbab9cc834d
Shervin Babaei <shervinb22@gmail.com>
2026-05-06 12:20:33 -0700

    feat: add workshops/workshopRoutes

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const profileRoute = require("./routes/profile");
 const registerCourseRoute = require("./routes/register"); // Course Registration
 const authRoutes = require("./routes/authRoutes"); // User login/signup
 const postsRoute = require("./routes/post");
+const workshopRoutes = require("./routes/workshopRoutes");
 
 const app = express();
 const PORT = 3000;
@@ -13,6 +14,7 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
+app.use("/api/workshops", workshopRoutes);
 
 // Connect to MongoDB Atlas
 async function startServer() {
