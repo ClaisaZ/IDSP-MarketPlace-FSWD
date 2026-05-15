ff45adf7fd469ceaee53615bc6cf4323e61d3e29
Clayton Liang <Claytonliang98@Hotmail.com>
2026-05-14 17:26:30 -0700

    Changed backend PORT for Render deployment

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const registerCourseRoute = require("./routes/register");
 const workshopRoutes = require("./routes/workshopRoutes");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
